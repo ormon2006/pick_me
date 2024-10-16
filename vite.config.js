6d7933503b676e12be33a5ee1b1ca4da3374ba99
Ormon <emametkulov@gmail.vom>
2024-10-17 01:25:22 +0600

    аы

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-// Получаем текущий путь файла
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'), 
-
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
