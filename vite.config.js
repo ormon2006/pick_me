c2cf0a384b7dac92f6b28389757511b02392f7c9
Ormon <emametkulov@gmail.vom>
2024-10-16 21:02:32 +0600

    Добавил alias и конфиг для jsconfig.json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import path from "path";
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// Получаем текущий путь файла
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-      alias: {
-      "@": path.resolve(__dirname, "./src"),
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'), 
+
     },
   },
 });
