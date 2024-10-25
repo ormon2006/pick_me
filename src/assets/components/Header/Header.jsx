d32c54fb5aac6250591ba0e55519793620d5ff31
ikoocyss <firuza220307@icloud.com>
2024-10-25 15:17:13 +0600

    м

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import styles from "@assets/components/Header/Header.module.scss"
+import logo from "@assets/image/Logo/logo.png"
 const Header = () => {
   return (
     <header>
-      <div className={styleslogo}>AthleteXpert.Ai</div>
-      <nav className={styles.nav}>
-        <a href="about">О нас</a>
-        <a href="services">Услуги</a>
-        <a href="rewievs">Отзывы</a>
-        <a href="contacts">Контакты</a>
-      </nav>
+      <div className="container">
+        <img src={logo} alt="" />
+        
+      </div>
     </header>
   )
 }
